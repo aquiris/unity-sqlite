3dc0bc5bda8cf439703e5b08fcbb10f6973083a7
Mauricio Rodrigues Cruz <brcruz@gmail.com>
2014-07-25 11:36:04 -0300

    Initial commit
    First stages of the tool with a better reademe.

---
+++ DBInterface.cs
@@ -0,0 +1,57 @@
+﻿using System.Data;
+using System.Data.Common;
+using Mono.Data.SqliteClient;
+using System.Collections.Generic;
+
+namespace aquiris.tools.dbinterface
+{
+	public class DBInterface {
+		public string Table{set{m_table = value;}}
+
+		private SqliteConnection m_connection;
+		private string m_database;
+		private string m_table;
+		private IDbCommand m_command;
+		private IDataReader m_reader;
+
+		public DBInterface(string p_database){
+			m_connection = new SqliteConnection("URI=file:" + p_database);
+			m_command = m_connection.CreateCommand();
+		}
+
+		public IDataReader Select(string p_column, string p_parameter){
+			Dictionary<string,string> result = new Dictionary<string, string>();
+			return ExecuteQuery("SELECT `"+p_column+"`,"+p_parameter+" FROM `"+m_table+"`  ORDER BY `rowid` ASC;");
+		}
+
+		public void Delete(string p_rowid){
+			ExecuteQuery("DELETE FROM `"+m_table+"` WHERE rowid="+p_rowid+";");
+		}
+
+		public void Update(int p_rowid, string p_column, string p_data){
+			ExecuteQuery("UPDATE `"+m_table+"` SET `"+p_column+"`="+p_data+" WHERE rowid="+p_rowid+";");
+		}
+
+		public void Insert(Dictionary<string, string> p_entries){
+			string keys = "";
+			string values = "";
+			foreach(KeyValuePair<string,string> pair in p_entries){
+				if(!string.IsNullOrEmpty(keys)){
+					keys += ",";
+					values += ",";
+				}
+				keys += "'" + pair.Key + "'";
+				values += "'" + pair.Value + "'";
+			}
+			ExecuteQuery("INSERT INTO `" + m_table + "`(" + keys + ") VALUES (" + values + ");");
+		}
+
+		public IDataReader ExecuteQuery(string p_query){
+			m_command.CommandText = p_query;
+			m_connection.Open();
+			m_reader = m_command.ExecuteReader();
+			m_connection.Close();
+			return m_reader;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 unity-sqlite
 ============
 
-Tools to integrate SQLite inside Unity3D game engine
+Simple tool to use SQLite inside the Unity3D game engine. Includes the necessary DLLs extracted from the Unity instalation and a small class with a simple interface;
+
+
+
