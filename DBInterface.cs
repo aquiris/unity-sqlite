cc7b35e49d9e880c6149808a84637ded3e469eb1
Mauricio Rodrigues Cruz <brcruz@gmail.com>
2014-07-29 17:52:37 -0300

    Table member is formated on setter

--- DBInterface.cs
+++ DBInterface.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 namespace Aquiris.Tools.DbInterface
 {
 	public class DbInterface {
-		public string Table{set{m_table = value;}}
+		public string Table{set{m_table = "["+value+"]";}}
 
 		private SqliteConnection m_connection;
 		private string m_database;
@@ -14,21 +14,23 @@ namespace Aquiris.Tools.DbInterface
 		private IDbCommand m_command;
 		private IDataReader m_reader;
 
-		public DbInterface(string p_database){
-			m_connection = new SqliteConnection("URI=file:" + p_database);
+		public DbInterface(string p_databaseFilePath, string p_table){
+			m_connection = new SqliteConnection("URI=file:" + p_databaseFilePath);
 			m_command = m_connection.CreateCommand();
+
+			Table = p_table;
 		}
 
-		public IDataReader Select(string p_column, string p_parameter){
-			return ExecuteQuery("SELECT ["+p_column+"],"+p_parameter+" FROM ["+m_table+"]  ORDER BY `rowid` ASC;");
+		public IDataReader Select(string p_select){
+			return ExecuteQuery("SELECT "+p_select+" FROM "+m_table+"  ORDER BY `rowid` ASC;");
 		}
 
 		public void Delete(string p_rowid){
-			ExecuteQuery("DELETE FROM ["+m_table+"] WHERE rowid="+p_rowid+";");
+			ExecuteQuery("DELETE FROM "+m_table+" WHERE rowid="+p_rowid+";");
 		}
 
 		public void Update(int p_rowid, string p_column, string p_data){
-			ExecuteQuery("UPDATE ["+m_table+"] SET \""+p_column+"\"=\""+p_data+"\" WHERE rowid="+p_rowid+";");
+			ExecuteQuery("UPDATE "+m_table+" SET \""+p_column+"\"=\""+p_data+"\" WHERE rowid="+p_rowid+";");
 		}
 
 		public void Insert(Dictionary<string, string> p_entries){
@@ -42,7 +44,7 @@ namespace Aquiris.Tools.DbInterface
 				keys += "'" + pair.Key + "'";
 				values += "'" + pair.Value + "'";
 			}
-			ExecuteQuery("INSERT INTO [" + m_table + "](" + keys + ") VALUES (" + values + ");");
+			ExecuteQuery("INSERT INTO " + m_table + "(" + keys + ") VALUES (" + values + ");");
 		}
 
 		public IDataReader ExecuteQuery(string p_query){
