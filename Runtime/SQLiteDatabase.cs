a6365d866b7707002412e22d429642b5d8e50c52
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2021-01-19 17:30:47 -0300

    Fixed project structure and dependencies;

---
+++ Runtime/SQLiteDatabase.cs
@@ -0,0 +1,15 @@
+﻿using Mono.Data.Sqlite;
+
+namespace Aquiris.SQLite
+{
+    public class SQLiteDatabase
+    {
+        private readonly SqliteConnection _connection = default;
+
+        public SQLiteDatabase(string databasePath)
+        {
+            
+            _connection = new SqliteConnection(databasePath);
+        }
+    }
+}
