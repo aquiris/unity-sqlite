7caf87f1ab8b4bb1a3ec67049cb3c8e398637f2b
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2021-01-20 19:05:10 -0300

    Added constants with reusable values;

---
+++ Runtime/Shared/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Aquiris.SQLite.Shared
+{
+    internal static class Constants
+    {
+        public static readonly string newLine = Environment.NewLine;
+        public static readonly string commaNewLine = $", {newLine}";
+    }
+}
