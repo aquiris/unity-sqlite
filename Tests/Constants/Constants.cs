c09ace7edac90b99d341e818763482b87c9b8ca3
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2021-01-20 11:10:08 -0300

    Moved the paths to a constants file;

---
+++ Tests/Constants/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Aquiris.SQLite.Tests.Constants
+{
+    public static class Constants
+    {
+        public static readonly string databaseParentPath = Path.Combine(Application.dataPath, "SQLite");
+        public static readonly string databaseFilePath = Path.Combine(databaseParentPath, "database.db");
+    }
+}
--- Tests/DatabaseCreationTests.cs
+++ Tests/DatabaseCreationTests.cs
@@ -1,35 +1,32 @@
 ﻿using System.IO;
 using Aquiris.SQLite;
+using Aquiris.SQLite.Tests.Constants;
 using NUnit.Framework;
-using UnityEngine;
 
 namespace Tests
 {
     public class DatabaseCreationTests
     {
-        private static readonly string path = Path.Combine(Application.dataPath, "SQLite");
-        private static readonly string databaseFilePath = Path.Combine(path, "database.db");
-
         [SetUp]
         public void SetUp()
         {
-            if (!File.Exists(databaseFilePath)) return;
-            File.Delete(databaseFilePath);
+            if (!File.Exists(Constants.databaseFilePath)) return;
+            File.Delete(Constants.databaseFilePath);
         }
         
         [Test]
         public void TestCreateDatabaseSuccess()
         {
-            CreateResult result = SQLiteDatabase.Create(databaseFilePath);
+            CreateResult result = SQLiteDatabase.Create(Constants.databaseFilePath);
             Assert.AreEqual(CreateResult.Create, result);
         }
 
         [Test]
         public void TestAlreadyExistingDatabase()
         {
-            CreateResult result = SQLiteDatabase.Create(databaseFilePath);
+            CreateResult result = SQLiteDatabase.Create(Constants.databaseFilePath);
             Assert.AreEqual(CreateResult.Create, result);
-            result = SQLiteDatabase.Create(databaseFilePath);
+            result = SQLiteDatabase.Create(Constants.databaseFilePath);
             Assert.AreEqual(CreateResult.AlreadyExists, result);
         }
     }
